c2eaffc54fc3bcbbb3d1954834c3d52c6c2f6995
Stanislav <skipper3191@yandex.ru>
2023-12-30 17:41:27 +0300

    Add Entitites: User, Category and Item

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+
+namespace WebApi.Entities
+{
+	public class Category
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+
+		public List<Item> Items { get; set; }
+
+		public string UserId { get; set; }
+		public User User { get; set; }
+	}
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using WebApi.Entities;
+
+namespace Domain.Entities
+{
+	public class User : IdentityUser
+	{
+		public List<Category> Categories { get; set; }
+	}
+}
