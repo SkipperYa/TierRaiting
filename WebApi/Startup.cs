74f03448470fe053d7024f263f89801c3ad890c3
Stanislav <skipper3191@yandex.ru>
2023-12-31 16:46:54 +0300

    fix merging

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -55,7 +55,6 @@ namespace WebApi
 			.AddTokenProvider<DataProtectorTokenProvider<User>>(TokenOptions.DefaultProvider);
 
 			services.AddMediator();
-			services.AddValidators();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
